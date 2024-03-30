b3301c20633a61e4ca46e6264a1e0c627a327002
maganga <88885384+Chrismaganga@users.noreply.github.com>
2024-03-30 23:55:30 +0200

    update

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import React from 'react';
+import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ZOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+import React from 'react';
+
+export default function RootLayout({ children }: React.PropsWithChildren<{}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: 'iconButton',
+            logoImageUrl: '/icons/yoom-logo.svg',
+          },
+          variables: {
+            colorText: '#fff',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1C1F2E',
+            colorInputBackground: '#252A41',
+            colorInputText: '#fff',
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
